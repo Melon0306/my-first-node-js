8c35b459574a002befd250579eac49c3aaf85903
Melon0306 <t.kily0121@gmail.com>
2019-02-09 10:38:37 +0000

    内容追加

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-'use strict';            //↓↓//0→node 1→ファイルのパスが入ってる。2は与える数値が入る
-const number = process.argv[2] || 0;//左が真ならnumberに代入、０やnull(偽は0代入。process.argvはNode.js がデフォルトで提供してくれる、コマンドラインの引数が入った配列
+'use strict';            //↓↓//0→nodeコマンドのファイルパス 1→実行しているプログラムファイルのパス。2は与える数値が入る
+const number = process.argv[2] || 0;//左が真ならnumberに代入、０やnull(偽)は0代入。
 //エラーでないよう0を代入させておく。↑↑　0などはfalsy
 let sum = 0;
 for (let i =1; i <=number; i++){
@@ -7,4 +7,6 @@ for (let i =1; i <=number; i++){
 }
 console.log(sum);
 
-//node app.js(ファイル名) 値　で実行
+// process.argvはNode.js がデフォルトで提供してくれる、コマンドラインの引数が入った配列
+//node app.js(ファイル名) 値　でJS実行
+//〇〇||△△　左側がtruthyなら左、falsyなら右を使う
